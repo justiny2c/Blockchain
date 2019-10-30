722febe3e3d0bf6f0fb18e8299d54942c7fabd1c
justiny2c <justiny2c@gmail.com>
2019-10-30 14:36:59 -0700

    Initial push. Blockchain file copied. Working on endpoints

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,12 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True).encode()
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +32,11 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f"{block_string}{proof}".encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    # TODO: change back to six
+    # sys.stdout.write(guess_hash[:3] + " ")
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
@@ -44,6 +53,7 @@ if __name__ == '__main__':
     f.close()
 
     # Run forever until interrupted
+    coins_mined = 0
     while True:
         r = requests.get(url=node + "/last_block")
         # Handle non-json response
@@ -56,13 +66,15 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = proof_of_work(data)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
+
+        coins_mined += 1
 
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
